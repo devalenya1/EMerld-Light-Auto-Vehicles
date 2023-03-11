7b63d4dd9b08e1e6adb3abf7c38b0caa10710062
devalenya <31144181+devalenya@users.noreply.github.com>
2023-03-11 14:22:35 +0100

    Update seller_details.dart

--- lib/screens/seller_details.dart
+++ lib/screens/seller_details.dart
@@ -143,6 +143,55 @@ class _SellerDetailsState extends State<SellerDetails> {
                       ),
                       child: buildCarouselSlider(context),
                     ),
+                    
+                    
+                    Divider(
+                      height: 1,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return CommonWebviewScreen(
+                            url:
+                                "#",
+                            page_name: "Donate To Association",
+                          );
+                        }));
+                      },
+                      child: Container(
+                        height: 40,
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(
+                            16.0,
+                            0.0,
+                            8.0,
+                            0.0,
+                          ),
+                          child: Row(
+                            children: [
+                              Text("Donate to Association",
+                                style: TextStyle(
+                                    color: MyTheme.font_grey,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              Spacer(),
+                              Icon(
+                                Ionicons.ios_add,
+                                color: MyTheme.font_grey,
+                                size: 24,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      height: 1,
+                    ),
+                    
+                    
                     Padding(
                       padding: const EdgeInsets.fromLTRB(
                         16.0,
