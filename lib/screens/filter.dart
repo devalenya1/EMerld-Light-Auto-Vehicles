232a91d9038ec5915bd5372d0619f64fca5bab1e
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2024-12-17 02:28:41 +0100

    17

--- lib/screens/filter.dart
+++ lib/screens/filter.dart
@@ -639,22 +639,7 @@ class _FilterState extends State<Filter> {
                                       Navigator.pop(context);
                                     },
                                   ),
-                                  RadioListTile(
-                                    dense: true,
-                                    value: "auction",
-                                    groupValue: _selectedSort,
-                                    activeColor: MyTheme.font_grey,
-                                    controlAffinity:
-                                        ListTileControlAffinity.leading,
-                                    title: "Auction",
-                                    onChanged: (value) {
-                                      setState(() {
-                                        _selectedSort = value;
-                                      });
-                                      _onSortChange();
-                                      Navigator.pop(context);
-                                    },
-                                  ),
+                                  
 
                                 ],
                               );
