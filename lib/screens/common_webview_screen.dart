13f351f0480987ac108118cdc986c8aac043daeb
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2024-12-19 09:00:22 +0100

    Update common_webview_screen.dart

--- lib/screens/common_webview_screen.dart
+++ lib/screens/common_webview_screen.dart
@@ -1,58 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import '../helpers/shared_value_helper.dart';
+import '../my_theme.dart';
 
 class CommonWebviewScreen extends StatefulWidget {
-  final String url;
-  final String page_name;
+  String url;
+  String page_name;
 
-  CommonWebviewScreen({Key key, this.url = "", this.page_name = ""})
-      : super(key: key);
+  CommonWebviewScreen({Key key, this.url = "", this.page_name = ""}) : super(key: key);
 
   @override
   _CommonWebviewScreenState createState() => _CommonWebviewScreenState();
 }
 
 class _CommonWebviewScreenState extends State<CommonWebviewScreen> {
-  InAppWebViewController _webViewController;
+  WebViewController _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return Directionality(
+      textDirection: app_language_rtl.$ ? TextDirection.rtl : TextDirection.ltr,
+      child: Scaffold(
         backgroundColor: Colors.white,
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.grey),
+        appBar: buildAppBar(context),
+        body: buildBody(),
+      ),
+    );
+  }
+
+  buildBody() {
+    return SizedBox.expand(
+      child: Container(
+        child: WebView(
+          debuggingEnabled: false,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (controller) {
+            _webViewController = controller;
+            _webViewController.loadUrl(widget.url);
+          },
+          onWebResourceError: (error) {},
+          onPageFinished: (page) {
+            //print(page.toString());
+          },
+          navigationDelegate: (NavigationRequest request) {
+            if (request.url.startsWith('file://')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+          initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.require_user_action_for_all_media_types,
+          allowFileURLs: true,
+          fileUpload: true, // Enable file upload
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      leading: Builder(
+        builder: (context) => IconButton(
+          icon: Icon(Icons.arrow_back, color: MyTheme.dark_grey),
           onPressed: () => Navigator.of(context).pop(),
         ),
-        title: Text(
-          widget.page_name,
-          style: TextStyle(fontSize: 16, color: Colors.blue),
-        ),
-        elevation: 0.0,
       ),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(url: Uri.parse(widget.url)),
-        initialOptions: InAppWebViewGroupOptions(
-          crossPlatform: InAppWebViewOptions(
-            mediaPlaybackRequiresUserGesture: false,
-            javaScriptEnabled: true,
-          ),
-        ),
-        onWebViewCreated: (controller) {
-          _webViewController = controller;
-          _webViewController.addJavaScriptHandler(
-            handlerName: 'fileUploadHandler',
-            callback: (args) {
-            // Custom handler logic if needed
-            },
-          );
-        },
-        
-        onConsoleMessage: (controller, consoleMessage) {
-          print(consoleMessage.message);
-        },
+      title: Text(
+        "${widget.page_name}",
+        style: TextStyle(fontSize: 16, color: MyTheme.accent_color),
       ),
+      elevation: 0.0,
+      titleSpacing: 0,
     );
   }
 }
