f5d6149d6f4afda251c8670a55621f2f4fd23b85
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2025-01-25 00:57:22 +0100

    477

--- lib/screens/common_webview_screen.dart
+++ lib/screens/common_webview_screen.dart
@@ -135,15 +135,15 @@ class _CommonWebviewScreenState extends State<CommonWebviewScreen> {
       backgroundColor: Colors.white,
       centerTitle: true,
       leading: Builder(
-        builder: (context) => IconButton(
-          icon: Icon(Icons.arrow_back, color: MyTheme.dark_grey),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
+        // builder: (context) => IconButton(
+        //   icon: Icon(Icons.arrow_back, color: MyTheme.dark_grey),
+        //   onPressed: () => Navigator.of(context).pop(),
+        // ),
       ),
-      title: Text(
-        widget.page_name,
-        style: TextStyle(fontSize: 16, color: MyTheme.accent_color),
-      ),
+      // title: Text(
+      //   widget.page_name,
+      //   style: TextStyle(fontSize: 16, color: MyTheme.accent_color),
+      // ),
       elevation: 0.0,
       titleSpacing: 0,
     );
