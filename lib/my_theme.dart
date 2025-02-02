4f4c2d8bbab2774145c7ffa238b182d1928e1143
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2025-02-02 13:15:23 +0100

    ryt

--- lib/my_theme.dart
+++ lib/my_theme.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 class MyTheme{
   /*configurable colors stars*/
-  static Color accent_color = Color.fromRGBO(229, 25, 25, 0.8);
-  static Color soft_accent_color = Color.fromRGBO(247,189,168, 1);
-  static Color splash_screen_color = Color.fromRGBO(229, 25, 25, 0.8); // if not sure , use the same color as accent color
+  // static Color accent_color = Color.fromRGBO(229, 25, 25, 0.8);
+  // static Color soft_accent_color = Color.fromRGBO(247,189,168, 1);
+  // static Color splash_screen_color = Color.fromRGBO(229, 25, 25, 0.8); // if not sure , use the same color as accent color
+  /*configurable colors ends*/
+  /*configurable colors stars*/
+  static Color accent_color = Color.fromRGBO(0,86,179, 1);
+  static Color accent_color_shadow = Color.fromRGBO(30, 144, 255, 0); // this color is a dropshadow of
+  static Color soft_accent_color = Color.fromRGBO(255, 215, 0, 1);
+  static Color splash_screen_color = Color.fromRGBO(0,86,179, 1); // if not sure , use the same color as accent color
   /*configurable colors ends*/
 
 
